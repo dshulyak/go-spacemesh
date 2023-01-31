27a370da0f455b3a2b8e8f175f89db72baed7894
Dmitry <yashulyak@gmail.com>
2023-01-31 20:25:01 +0200

    add nonces

--- post/post.go
+++ post/post.go
@@ -4,6 +4,7 @@ import (
 	"encoding/binary"
 	"errors"
 	"io"
+	"math"
 	"os"
 
 	"github.com/minio/sha256-simd"
@@ -11,9 +12,12 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-const k1 = 2000      // irrelevant here
-const k2 = 1800      // number of collected results
-const difficulty = 1 // just to go through the file
+func provingDifficulty(numLabels uint64, k1 uint64) uint64 {
+	const maxTarget = math.MaxUint64
+	x := maxTarget / numLabels
+	y := maxTarget % numLabels
+	return x*k1 + (y*k1)/numLabels
+}
 
 func setup(filename string) (*os.File, error) {
 	f, err := os.Open(filename)
@@ -30,7 +34,7 @@ func setup(filename string) (*os.File, error) {
 	return f, nil
 }
 
-func Prove(cpu int, filename string) error {
+func Prove(cpu int, filename string, k1, k2 uint64) error {
 	f, err := setup(filename)
 	if f != nil {
 		defer f.Close()
@@ -39,29 +43,36 @@ func Prove(cpu int, filename string) error {
 		return err
 	}
 	var (
-		proof = make(chan uint64, k2)
-		eg    errgroup.Group
+		proof      = make(chan uint64, k2)
+		eg         errgroup.Group
+		share      = 80 / cpu
+		difficulty = provingDifficulty(512<<20, k1)
 	)
 	for i := 0; i < cpu; i++ {
+		i := 0
 		eg.Go(func() error {
+			start := share * i
+			end := share * (i + 1)
 			buf := make([]byte, 1<<20)
 			input := make([]byte, 37)
 			copy(input, "any challenge")
-			binary.BigEndian.PutUint32(input[32:], 101)
 			for {
 				n, err := f.Read(buf)
 				if err != nil && !errors.Is(err, io.EOF) {
 					return err
 				}
-				// one byte label
-				for _, b := range buf[:n] {
-					input[36] = b
-					r := sha256.Sum256(input)
-					if r2 := binary.LittleEndian.Uint64(r[:]); r2 <= difficulty {
-						select {
-						case proof <- r2:
-						default:
-							return nil
+				for nonce := start; nonce <= end; nonce++ {
+					// one byte label
+					for _, b := range buf[:n] {
+						binary.BigEndian.PutUint32(input[32:], uint32(nonce))
+						input[36] = b
+						r := sha256.Sum256(input)
+						if r2 := binary.LittleEndian.Uint64(r[:]); r2 <= difficulty {
+							select {
+							case proof <- r2:
+							default:
+								return nil
+							}
 						}
 					}
 				}
--- post/post_test.go
+++ post/post_test.go
@@ -7,7 +7,7 @@ import (
 func BenchmarkProof(b *testing.B) {
 	b.SetBytes(512 << 20)
 	for i := 0; i < b.N; i++ {
-		if err := Prove(8, "/tmp/example"); err != nil {
+		if err := Prove(10, "/tmp/example", 200, 180); err != nil {
 			b.Fatal(err)
 		}
 	}
